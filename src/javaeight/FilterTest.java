652ba3b4abcea7991e47cf8de7ee92a21e8d4c94
aswal94 <aswal.amit94@gmail.com>
2021-04-30 18:56:10 +0530

    Add basic java 8 examples

---
+++ src/javaeight/FilterTest.java
@@ -0,0 +1,122 @@
+package javaeight;
+
+import java.util.*;
+import java.util.logging.Filter;
+import java.util.stream.Collectors;
+
+public class FilterTest {
+    public static void main(String[] args) {
+        new FilterTest().test();
+    }
+
+    public void test() {
+        populateEmployees();
+    }
+
+    List<Employee> employeeList;
+
+    public void populateEmployees() {
+        employeeList = new ArrayList<>();
+        employeeList.add(new Employee("Amit Aswal", 20));
+        employeeList.add(new Employee("Dheeraj Pundir", 23));
+        employeeList.add(new Employee("Pappu Bahadur", 21));
+        employeeList.add(new Employee("Anuj Rana", 20));
+        employeeList.add(new Employee("Amit Rawat", 23));
+
+        // filter list age > 20 then print names
+        List<String> filteredList = employeeList.stream()
+                .filter(employee -> employee.getAge() > 20)
+                .map(Employee::getName) // method reference
+                .collect(Collectors.toList());
+
+        System.out.println(filteredList);
+
+        // same (another approach)
+        employeeList.stream()
+                .filter(employee -> employee.getAge() > 20)
+                .map(Employee::getName) // method reference
+                .forEach(name -> System.out.println(name)); // consumer function
+
+
+        //------------------------------
+        // count employe with age > 20
+        long count = employeeList.stream()
+                .filter(e -> e.getAge() > 20)
+                .count();
+        System.out.println("Employe with age greater than 20: " + count);
+
+        //---------------
+        // find employee with name dheeraj
+        Optional<Employee> e = employeeList.stream()
+                .filter(emp -> emp.getName().equalsIgnoreCase("Dheeraj Pundir"))
+                .findAny();
+
+        if (e.isPresent()) {
+            System.out.println("Found: " + e.get().getName());
+        }
+
+        // --------------
+        // find maximum age of employee
+        OptionalInt max = employeeList.stream()
+                .mapToInt(Employee::getAge)
+                .max();
+
+        if (max.isPresent()) {
+            System.out.println("Max age: " + max.getAsInt());
+        }
+
+        // ------------
+        // sort on basis of age
+
+        System.out.println("Sorted by age");
+        employeeList.sort((e1, e2) -> e1.getAge() - e2.getAge());
+        employeeList.forEach(ex -> System.out.println(ex.getName() + ", " + ex.getAge()));
+
+        // -------------------
+        // join employees names with ","
+
+        List<String> emloyeesNameList = employeeList.stream()
+                .map(Employee::getName)
+                .collect(Collectors.toList());
+        System.out.println("Names: " + String.join(",", emloyeesNameList));
+
+        //--------------------------
+        // given list of numbers square them -> filter them (>1000) -> find Average
+        Integer[] arr = new Integer[]{100, 23, 13, 44, 53, 225, 40, 115};
+        List<Integer> integerList = Arrays.asList(arr);
+        OptionalDouble average = integerList.stream()
+                .mapToInt(integer -> integer * integer)
+                .filter(integer -> integer > 1000)
+                .average();
+
+        if (average.isPresent()) {
+            System.out.println(average.getAsDouble());
+        }
+
+        // --------------
+        // remove duplicates from an integer list
+        // way 1 -> using set
+        Integer[] xarr = new Integer[]{1, 2, 3, 2, 1, 4 ,5,1};
+
+        List<Integer> listWithDuplicates = Arrays.asList(xarr);
+        Set<Integer> setWithoutDup = listWithDuplicates.stream()
+                .collect(Collectors.toSet());
+
+        setWithoutDup.forEach(i -> System.out.println(i));
+
+        // another way without using set
+        List<Integer> noDup = listWithDuplicates.stream().distinct().collect(Collectors.toList());
+                noDup.forEach(i -> System.out.println(i));
+        //--------------
+        // grouping elements by name
+
+        Map<String, List<Employee>> empMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getName));
+
+        empMap.forEach((name, empList) -> {
+            System.out.print(name + ", ");
+            empList.stream().forEach(exx -> System.out.println(exx.getName()));
+        });
+
+    }
+}
